298d5a56808342889a4bbbd7d9e4048ac49458b3
Rasyid <rasydridla298@gmail.com>
2021-11-15 19:32:00 +0700

    added splash screen

---
+++ app/src/main/java/id/rrdev/jetpackpro/ui/splash/SplashActivity.kt
@@ -0,0 +1,25 @@
+package id.rrdev.jetpackpro.ui.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import id.rrdev.jetpackpro.R
+import id.rrdev.jetpackpro.ui.MainActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        setupView()
+    }
+
+    private fun setupView() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            finish()
+        }, 2000)
+    }
+}
